095293986436a6fd444ba491e2d8c24ea72cd331
bsh <15811437140@163.com>
2020-07-03 10:15:31 +0800

    修改登录

--- src/config/index.js
+++ src/config/index.js
@@ -1,4 +1,4 @@
 module.exports = {
-  baseUrl: 'http://www.adminvue.com'
-  // baseUrl: 'https://zhixiaosheapi.soft-in-hard.vip'
+  // baseUrl: 'http://www.adminvue.com'
+  baseUrl: 'https://zhixiaosheapi.soft-in-hard.vip'
 }
