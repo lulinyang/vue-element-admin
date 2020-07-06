5d3f73551c454cc6043b449e901b823066bc2d4d
bsh <15811437140@163.com>
2020-07-06 09:40:49 +0800

    修改路由

--- src/router/index.js
+++ src/router/index.js
@@ -5,104 +5,82 @@ Vue.use(Router);
 
 export default new Router({
   routes: [{
-      path: '/',
-      redirect: '/home'
+    path: '/',
+    redirect: '/home'
+  },
+  {
+    path: '/',
+    component: () => import('../components/common/Home.vue'),
+    children: [{
+      path: '/home',
+      component: resolve => require(['@/view/common/BaseTable.vue'], resolve),
+      name: "系统首页"
     },
     {
-      path: '/',
-      component: () => import('../components/common/Home.vue'),
-      children: [{
-          path: '/home',
-          component: resolve => require(['@/view/common/BaseTable.vue'], resolve),
-          meta: {
-            title: '系统首页'
-          }
-        },
-        {
-          path: '/home/user/list',
-          component: resolve => require(['@/view/Account/AdminUser/list.vue'], resolve),
-          meta: {
-            title: '管理员列表'
-          }
-        },
-        {
-          path: '/home/menu/list',
-          component: resolve => require(['@/view/Account/Menu/list.vue'], resolve),
-          meta: {
-            title: '菜单管理'
-          }
-        },
-        {
-          path: '/home/menu/edit/:id',
-          name: 'menuEdit',
-          component: resolve => require(['@/view/Account/Menu/edit.vue'], resolve),
-          meta: {
-            title: '编辑菜单'
-          }
-        },
-        {
-          path: '/home/rule/list',
-          component: resolve => require(['@/view/Account/Rule/list.vue'], resolve),
-          meta: {
-            title: '权限规则'
-          }
-        },
-        {
-          path: '/home/rule/add',
-          component: resolve => require(['@/view/Account/Rule/add.vue'], resolve),
-          meta: {
-            title: '添加权限'
-          }
-        },
-        {
-          path: '/home/groups/list',
-          component: resolve => require(['@/view/Account/Group/list.vue'], resolve),
-          meta: {
-            title: '用户组管理'
-          }
-        },
-        {
-          path: '/neditor',
-          component: resolve => require(['@/view/NEditor/index.vue'], resolve),
-          meta: {
-            title: '富文本'
-          }
-        },
-        {
-          path: '/neditor',
-          component: resolve => require(['@/view/NEditor/index.vue'], resolve),
-          name: '富文本'
-        },
-        {
-          path: '/excel',
-          component: resolve => require(['@/view/Excel/index.vue'], resolve),
-          name: 'Excel'
-        },
-        {
-          path: '/404',
-          component: () => import('@/view/common/404.vue'),
-          name: '404'
-        },
-        {
-          path: '/403',
-          component: () => import('@/view/common/403.vue'),
-          name: '403'
-        }
-      ]
+      path: '/home/user/list',
+      component: resolve => require(['@/view/Account/AdminUser/list.vue'], resolve),
+      name: '管理员列表'
     },
     {
-      path: '/login',
-      component: () => import('@/view/login.vue'),
-      name: '登录'
+      path: '/home/menu/list',
+      component: resolve => require(['@/view/Account/Menu/list.vue'], resolve),
+      name: '菜单管理'
     },
     {
-      path: '*',
-      redirect: '/404'
+      path: '/home/menu/edit/:id',
+      component: resolve => require(['@/view/Account/Menu/edit.vue'], resolve),
+      name: '编辑菜单',
+    },
+    {
+      path: '/home/rule/list',
+      component: resolve => require(['@/view/Account/Rule/list.vue'], resolve),
+      name: '权限规则'
+    },
+    {
+      path: '/home/rule/add',
+      component: resolve => require(['@/view/Account/Rule/add.vue'], resolve),
+      name: '添加权限'
+    },
+    {
+      path: '/home/groups/list',
+      component: resolve => require(['@/view/Account/Group/list.vue'], resolve),
+      name: '用户组管理'
+    },
+    {
+      path: '/neditor',
+      component: resolve => require(['@/view/NEditor/index.vue'], resolve),
+      name: '富文本'
+    },
+    {
+      path: '/excel',
+      component: resolve => require(['@/view/Excel/index.vue'], resolve),
+      name: 'Excel'
+    },
+    {
+      path: '/404',
+      component: () => import('@/view/common/404.vue'),
+      name: '404'
+    },
+    {
+      path: '/403',
+      component: () => import('@/view/common/403.vue'),
+      name: '403'
     }
+    ]
+  },
+  {
+    path: '/login',
+    component: () => import('@/view/login.vue'),
+    name: '登录'
+  },
+  {
+    path: '*',
+    redirect: '/404'
+  }
   ]
 });
 
 const originalPush = Router.prototype.push
-   Router.prototype.push = function push(location) {
-   return originalPush.call(this, location).catch(err => err)
+Router.prototype.push = function push(location) {
+  return originalPush.call(this, location).catch(err => err)
 }
